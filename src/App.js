affb353b795c61188d2c17a6b28ccbec23ccee70
yalfarosullivan@yahoo.com <Yara Alfaro-Sullivan>
2022-12-19 23:44:14 -0800

    completed activity

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react'
+// Make sure to import the component we just built:
+import ColorBlock from './ColorBlock'
+import ColorForm from './ColorForm'
+
+
+
+
+function App() {
+  let [colors, setColors] = useState ([
+    'violet', 'blue',
+    'lightblue', 'green',
+    'greenyellow', 'yellow',
+    'orange', 'red'
+  ])
+
+  const addColor = (newColor) => {
+  setColors([...colors, newColor])
+  }
+  return (
+    <div className="App">
+      {colors.map(( color, i) => 
+      <ColorBlock key={i} color={color} />)}
+      <ColorForm addColor={addColor}/>
+  </div>
+  )
+}
+
+
+export default App;
